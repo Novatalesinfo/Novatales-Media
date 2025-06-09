33a9889dc53cbd461722287f55add38bc7aaee1b
himanshu-novatales <himanshu.m@novatales.com>
2025-06-09 19:03:15 +0530

    `Refactor RealEstateForm component: update axios post request and add margin to container div`

--- src/components/realEstateFrom.js
+++ src/components/realEstateFrom.js
@@ -62,13 +62,13 @@ const RealEstateBookFrom = () => {
         try {
             setLoading(true);
             const formWithPage = { ...formData, page: window.location.href };
-             axios.post("/realestate.php", formWithPage).then(
+            axios.post("/realestate.php", formWithPage).then(
                 res => {
                     setFormData(initialFormData);
                     setConsent(false);
                     navigate("/thankyou");
                 }
-             )                                                    
+            )
         } catch (error) {
             console.error('Error:', error);
             toast.error(
@@ -90,8 +90,10 @@ const RealEstateBookFrom = () => {
         }
     }
     return (
-        <div className="service-content LogoVisual d-flex justify-content-center align-items-center extraPadding mb-2 " >
-             <div className="extraPadding" style={{
+        <div style={{
+            marginTop: "60px"
+        }} className="service-content LogoVisual d-flex justify-content-center align-items-center extraPadding  mb-2 pt-4 " >
+            <div className="extraPadding" style={{
                 backgroundColor: "#f9f9f9", padding: "30px 0px",
                 border: "1px dashed #fec436",
                 borderRadius: "35px",
